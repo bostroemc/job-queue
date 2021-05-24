e5dfd4c49deccf178d43f762b2013fca896f40db
Carl Bostroem <cpbostrom@yahoo.com>
2021-05-24 13:32:01 -0500

    update 1.0.6

--- datalayerprovider/my_provider_node.py
+++ datalayerprovider/my_provider_node.py
@@ -167,7 +167,7 @@ class Node:
     def __on_read(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
         print("bostroemc: count __on_read", len(self.queue))
         new_data = Variant()
-        new_data.set_string(str(len(self.queue)))
+        new_data.set_uint32(len(self.queue))
         cb(Result(Result.OK), new_data)
     
     def __on_write(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
